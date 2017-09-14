28bee21d39de41a06a73553faf475e8bf3508f12
Annoynamous72z <ryanshi1999@gmail.com>
2017-09-14 13:24:49 -0400

    Made some stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$( document ).ready(function() {
+	$(".intro").click(function(){
+		$(".intro").animate({left: '1920'},function(){
+			$(".intro").hide();
+		})
+		$(".middle").animate ({left: '1920'},function(){
+		$(".middle").hide();
+		})
+	});	
+});
+
+
 
