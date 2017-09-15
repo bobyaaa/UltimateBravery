2063362044e1cae5cc15a814e744e421c6c323b2
Andrew Xing <xingandrew1@gmail.com>
2017-09-15 09:43:08 -0400

    Updates and slide animations

--- js/main.js
+++ js/main.js
@@ -1,11 +1,66 @@
 $(document).ready(function(){
+	var $bravery = true;
+	var $about = false;
+	var $github = false;
 
-	$(".config").hide();
+	$(".bravery").on('click', function() {
+		//Write code to reset all data
 
-	$(".intro").on('click', function(){
-		$(".intro").hide();
-		$(".slide").animate({width:'toggle'},350); 
+
+
+		if ($about == true) {
+			$(".intro").fadeIn('slow');
+			$(".slide1").animate({height:'toggle'},350);
+		} else if ($github == true) {
+			$(".intro").fadeIn("slow");
+			$(".slide1").animate({height:'toggle'}, 350);
+			$(".slide2").animate({height:'toggle'}, 350);
+		}
+
+		$bravery = true;
+		$about = false;
+		$github = false;
 	});
+
+	$(".intro").on('click', function(){ 
+		//Everything works until we press the make me brave button!! NICE MEME.
+		$bravery = false;
+		$about = true;
+		$github = false;
+
+		$(".intro").hide();
+		$(".slide1").animate({width:'toggle'},350); 
+	});
+
+	$(".about").on('click', function() {
+
+		if ($bravery == true) {
+			$(".intro").fadeOut('slow');
+			$(".slide1").animate({height:'toggle'}, 350);
+		} else if ($github == true) {
+			$(".slide2").animate({height:'toggle'}, 350);
+		}
+
+		$about = true;
+		$bravery = false;
+		$github = false;
+
+	});
+
+	$(".github").on('click', function() {
+
+		if ($bravery == true) {
+			$(".intro").fadeOut('slow');
+			$(".slide1").animate({height:'toggle'}, 350);
+			$(".slide2").animate({height:'toggle'}, 350);
+		} else if ($about == true) {
+			$(".slide2").animate({height:'toggle'}, 350);
+		} 
+
+		$github = true;
+		$bravery = false;
+		$about = false;
+	}); 
 
 
 
