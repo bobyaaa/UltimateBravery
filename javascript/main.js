b45938be3abdc2e7d345766204715ce6ced248e1
Annoynamous72z <ryanshi1999@gmail.com>
2017-09-18 17:32:51 -0400

    added next button

--- javascript/main.js
+++ javascript/main.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
 	var $bravery = true;
 	var $about = false;
 	var $github = false;
-
+	$(".next").fadeOut('fast');
 	$(".bravery").on('click', function() {
 		//Write code to reset all data
-
-
 
 		if ($about == true) {
 			$(".intro").fadeIn('slow');
@@ -18,7 +16,9 @@ $(document).ready(function(){
 		}
 			$(".progress1").css("background-color","grey");
 		$(".progress1").css("box-shadow", "0px 0px 0px #2390c6");
-
+		$(".progress2").css("background-color","grey");
+		$(".progress2").css("box-shadow", "0px 0px 0px #2390c6");
+		$(".next").fadeOut('fast');
 		$bravery = true;
 		$about = false;
 		$github = false;
@@ -34,6 +34,7 @@ $(document).ready(function(){
 		$(".slide1").animate({width:'toggle'},350); 
 		$(".progress1").css("background-color","#2390c6");
 		$(".progress1").css("box-shadow", "0px 0px 15px #2390c6");
+		$(".next").fadeIn('slow');
 	});
 
 	$(".about").on('click', function() {
@@ -47,7 +48,9 @@ $(document).ready(function(){
 		}
 		$(".progress1").css("background-color","grey");
 		$(".progress1").css("box-shadow", "0px 0px 0px #2390c6");
-
+		$(".next").fadeOut('fast');
+		$(".progress2").css("background-color","grey");
+		$(".progress2").css("box-shadow", "0px 0px 0px #2390c6");
 		$about = true;
 		$bravery = false;
 		$github = false;
@@ -65,12 +68,18 @@ $(document).ready(function(){
 		} 
 			$(".progress1").css("background-color","grey");
 		$(".progress1").css("box-shadow", "0px 0px 0px #2390c6");
-
+		$(".next").fadeOut('fast');
+		$(".progress2").css("background-color","grey");
+		$(".progress2").css("box-shadow", "0px 0px 0px #2390c6");
 		$github = true;
 		$bravery = false;
 		$about = false;
 	}); 
 
+	$(".next").on('click',function(){
+		$(".progress2").css("background-color","#2390c6");
+		$(".progress2").css("box-shadow", "0px 0px 15px #2390c6");
+	});
 
 
 	//How about this, so when I press the button, the opacity of the background changes into 1 (fade/animate it in if possible)
